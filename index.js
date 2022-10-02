0ff8cfef2cfd38812dd6f80c2ecd68c1316c8be3
Patricio <107218376+Prgm-code@users.noreply.github.com>
2022-10-02 04:55:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let request = require('request');
 
 const dnsUpdater = async function () {
-   await request('https://dynamicdns.park-your-domain.com/update?host=@&domain=hardpro.store&password=[YOUR_DYNAMIC_PASSWORD]&ip=', function (error, response, body) {
+   await request('https://dynamicdns.park-your-domain.com/update?host=@&domain=[YOUR.DOMAIN}&password=[YOUR_DYNAMIC_PASSWORD]&ip=', function (error, response, body) {
     if (!error && response.statusCode == 200) {
       console.log(body) // Show the HTML for the Google homepage. 
     }
