9fdcf28ef9b351a04eeabf3fcc4930171f536c5f
Patricio Gómez <hardpro.ng@gmail.com>
2022-10-02 21:38:32 -0300

    agregado dotenv

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-let request = require('request');
+const request = require('request');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const dnsUpdater = async function () {
-   await request('https://dynamicdns.park-your-domain.com/update?host=@&domain=[YOUR.DOMAIN}&password=[YOUR_DYNAMIC_PASSWORD]&ip=', function (error, response, body) {
+   await request(`https://dynamicdns.park-your-domain.com/update?host=*&domain=${process.env.YOUR_DOMAIN}&password=${process.env.YOUR_DYNAMIC_PASSWORD}&ip=`, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       console.log(body) // Show the HTML for the Google homepage. 
     }
@@ -22,7 +24,11 @@ if (updateStart) {
   dnsUpdater();
   updateStart=false;
 }
-setInterval(dnsUpdater, 1000*60*2);
+let time = parseInt(process.env.YOUR_UPDATE_INTERVAL);
+console.log("Update interval is "+time+" minutes"); 
+setInterval(dnsUpdater, 1000*60* time);
+
+
 
 
 
