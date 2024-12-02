a969ec1018b16082b66256d47139fdfb386e0be6
Patricio Gómez Meneses <107218376+Prgm-code@users.noreply.github.com>
2024-12-02 18:31:39 -0300

    Update index.js
    YOUR_HOST

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const dnsUpdater = async function () {
-   await request(`https://dynamicdns.park-your-domain.com/update?host=*&domain=${process.env.YOUR_DOMAIN}&password=${process.env.YOUR_DYNAMIC_PASSWORD}&ip=`, function (error, response, body) {
+   await request(`https://dynamicdns.park-your-domain.com/update?host=${process.env.YOUR_HOST}&domain=${process.env.YOUR_DOMAIN}&password=${process.env.YOUR_DYNAMIC_PASSWORD}&ip=`, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       console.log(body) // Show the HTML for the Google homepage. 
     }
